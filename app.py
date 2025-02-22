b8267478d1b37eb0c4b3a2fc968e8f58a24cc967
Yassine El-jdid <eljdidyassine@gmail.com>
2025-02-22 11:35:02 +0100

    updated

--- app.py
+++ app.py
@@ -2,34 +2,55 @@ import pandas as pd
 import os
 import sys
 from dotenv import load_dotenv
+import argparse
 load_dotenv()
 
-from gpt_evaluation import GPT_Evaluation
+from gpt_evaluation import GPT_Evaluation, MetricType
+
+arg_parser = argparse.ArgumentParser("GPT Evaluation")
+arg_parser.add_argument("-d", "--dataframe", action="store_true", help="-f or --dataframe to Evaluate DataFrame")
+arg_parser.add_argument("-p", "--plot_code", action="store_true", help="-p or --plot_code to Evaluate Plot Code")
+arg_parser.add_argument("-s", "--sql", action="store_true", help="-s or --sql to Evaluate SQL")
+args = arg_parser.parse_args()
 
 dir = "./data"
-GPT = str(sys.argv[1]).upper()
-
 os.makedirs(os.path.join(dir, "DF"), exist_ok=True)
 os.makedirs(os.path.join(dir, "SQL"), exist_ok=True)
 
+GPT = "DF" if args.dataframe else "SQL" if args.sql else None
+PLOT_CODE = True if args.plot_code else False
 gpt_evaluation = GPT_Evaluation()
 if GPT == "DF":
     gpt_evaluation.url = os.getenv("DF_URL")
     for file_name in os.listdir(os.path.join(dir, GPT)):
         gpt_evaluation.type = GPT
-        if file_name.endswith((".csv", ".xlsx")):
+        if file_name.endswith((".csv", ".xlsx")) and file_name == "Regional_Tourism_Indicators.csv":
+            dataset_name = f"{GPT} {file_name}"
             try:
-                json_qa = os.path.join(dir, GPT, file_name.replace(".csv", ".json").replace(".xlsx", ".json"))
-                print(json_qa)
-                df = pd.read_json(json_qa)
-                df = df.rename(columns={"question": "input_question", "answer": "output_answer"})
-                gpt_evaluation.store_data(df, f"{GPT} Dataset {file_name}")
-                if file_name.endswith(".csv"):
-                    df = pd.read_csv(os.path.join(dir, GPT, file_name))
+                if PLOT_CODE:
+                    dataset_name = f"{GPT} Plot {file_name}"
+                    json_qa_plot = os.path.join(dir, GPT, file_name.replace(".csv", "_plot.json").replace(".xlsx", "_plot.json"))
+                    df_plot = pd.read_json(json_qa_plot)
+                    df_plot = df_plot.rename(columns={"question": "input_question", "answer": "output_answer"})
+                    gpt_evaluation.store_data(df_plot, dataset_name)
+                    
+                    if file_name.endswith(".csv"):
+                        df = pd.read_csv(os.path.join(dir, GPT, file_name))
+                    else:
+                        df = pd.read_excel(os.path.join(dir, GPT, file_name))
+                    gpt_evaluation.fill_context(df, file_name)
+                    gpt_evaluation.evaluate(dataset_name, MetricType.PLOT.value)
                 else:
-                    df = pd.read_excel(os.path.join(dir, GPT, file_name))
-                gpt_evaluation.fill_context(df, file_name)
-                gpt_evaluation.evaluate(f"{GPT} Dataset {file_name}")
+                    json_qa = os.path.join(dir, GPT, file_name.replace(".csv", ".json").replace(".xlsx", ".json"))
+                    df = pd.read_json(json_qa)
+                    df = df.rename(columns={"question": "input_question", "answer": "output_answer"})
+                    gpt_evaluation.store_data(df, dataset_name)
+                    if file_name.endswith(".csv"):
+                        df = pd.read_csv(os.path.join(dir, GPT, file_name))
+                    else:
+                        df = pd.read_excel(os.path.join(dir, GPT, file_name))
+                    gpt_evaluation.fill_context(df, file_name)
+                    gpt_evaluation.evaluate(dataset_name, MetricType.NORMAL.value)
             except Exception as e:
                 raise e
         else:
@@ -38,5 +59,3 @@ elif GPT == "SQL":
     URL = os.getenv("SQL_URL")
 else:
     raise ValueError("GPT not recognized")
-        
-        
--- gpt_evaluation.py
+++ gpt_evaluation.py
@@ -1,8 +1,11 @@
 from langchain_community.llms import Replicate
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 from langsmith import Client
+from langchain_openai import ChatOpenAI
 from datetime import datetime
+from enum import Enum
+from pydantic import BaseModel, Field
 import pandas as pd
 import requests
 import json
@@ -10,16 +13,30 @@ import os
 
 from constants import PAYLOAD
 
+class MetricType(str, Enum):
+    NORMAL = "NORMAL"
+    PLOT = "PLOT"
+
+class PlotCodeEval(BaseModel):
+    Comparison: bool = Field(..., description="Whether they produce the same output or not")
+    Reasoning: str = Field(..., description="Reasoning for the comparison")
+
 class GPT_Evaluation:
     def __init__(self):
         self.client = Client()
         self.payload = PAYLOAD
         self.url = ""
         self.type = ""
-        self.llm = Replicate(
+        self.llm_openai = ChatOpenAI(
+            model="gpt-4o",
+            temperature=0
+        )
+        self.llm_llama = Replicate(
             model="meta/meta-llama-3.1-405b-instruct",
             model_kwargs=dict(temperature=0),
         )
+        
+        self.model = self.llm_llama
     
     def fill_context(self, df_data: pd.DataFrame = None, file_name = None) -> str:
         if self.type == "DF":
@@ -65,17 +82,24 @@ class GPT_Evaluation:
     def generate_new_answer(self, question: str) -> str:
         self.payload["query"] = question
         result = requests.post(self.url + "/chat", json=self.payload).json()
-        return result.get("text")
+        return {
+            "answer": result.get("text"),
+            "plot_code": json.loads(result.get("context")[0])['plot_code']
+        }
 
     def final_answer_eval(self, inputs: dict, outputs: dict, reference_outputs: dict) -> bool:
+        question = inputs['question'].replace("{", "{{").replace("}", "}}")
+        real_answer = reference_outputs['answer'].replace("{", "{{").replace("}", "}}")
+        generated_answer = outputs['response'].replace("{", "{{").replace("}", "}}")
+        
         eval_instructions = "You are an expert professor specialized in grading student's answers to questions."
         user_content = (
             "You are grading the following question:\n"
-            f"{inputs['question']}\n"
+            f"{question}\n"
             "Here is the real answer:\n"
-            f"{reference_outputs['answer']}\n"
+            f"{real_answer}\n"
             "You are grading the following predicted answer:\n"
-            f"{outputs['response']}\n"
+            f"{generated_answer}\n"
             "Respond Only with CORRECT or INCORRECT, No additional texts or comments.\n"
         )
 
@@ -84,22 +108,64 @@ class GPT_Evaluation:
             ("user", user_content)
         ])
 
-        chain = prompt | self.llm | StrOutputParser()
+        chain = prompt | self.model | StrOutputParser()
         response = chain.invoke({})
         return response == "CORRECT"
     
-    def target(self, inputs: str) -> dict:
-        return {"response": self.generate_new_answer(inputs["question"])}
-    
-    def evaluate(self, dataset_name: str, print_results: bool = True):
-        os.makedirs("results", exist_ok=True)
-        results = self.client.evaluate(
-            self.target,
-            data=dataset_name,
-            evaluators=[
-                self.final_answer_eval
-            ],
-            experiment_prefix=f"eval-{self.type}-{datetime.now().strftime('%Y%m%d')}"
+    def plot_code_eval(self, inputs: dict, outputs: dict, reference_outputs: dict) -> bool:
+        question = inputs['question']
+        real_answer = reference_outputs['answer'].replace("{", "{{").replace("}", "}}")
+        generated_answer = outputs['response'].replace("{", "{{").replace("}", "}}")
+        
+        eval_instructions = "You are an expert Python programmer and code evaluator. Your task is to compare two pieces of Python code and determine whether they produce the same output and check if they do the same Conditional Filtering.\n"
+        user_content = (
+            "[BEGIN DATA]\n************\n"
+            f"[Question]: {question}\n"
+            "************\n"
+            f"[Real Answer]: {real_answer}\n"
+            "************\n"
+            f"[Generated Answer]: {generated_answer}\n"
+            "[END DATA]\n"
+            "Respond Only with CORRECT or INCORRECT, No additional texts or comments."
         )
-        if print_results:
+
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", eval_instructions),
+            ("user", user_content)
+        ])
+
+        chain = prompt | self.model | StrOutputParser()
+        response = chain.invoke({})
+        return response == "CORRECT"
+    
+    def normal_target(self, inputs: str) -> dict:
+        answer = self.generate_new_answer(inputs["question"])['answer']
+        return {"response": answer}
+    
+    def plot_target(self, inputs: str) -> dict:
+        plot_code = self.generate_new_answer(inputs["question"])['plot_code']
+        return {"response": plot_code}
+    
+    def evaluate(self, dataset_name: str, type: MetricType = MetricType.NORMAL, print_results: bool = True):
+        os.makedirs("results", exist_ok=True)
+        
+        result = None
+        if type == MetricType.NORMAL.value:
+            results = self.client.evaluate(
+                self.normal_target,
+                data=dataset_name,
+                evaluators=[self.final_answer_eval],
+                experiment_prefix=f"Normal-eval-{self.type}-{datetime.now().strftime('%Y%m%d')}"
+            )
+        elif type == MetricType.PLOT.value:
+            results = self.client.evaluate(
+                self.plot_target,
+                data=dataset_name,
+                evaluators=[self.plot_code_eval],
+                experiment_prefix=f"Plot-eval-{self.type}-{datetime.now().strftime('%Y%m%d')}"
+            )
+        else:
+            raise ValueError("Metric type not recognized")
+
+        if print_results and results:
             results.to_pandas().to_csv(f"results/eval-{dataset_name}-{datetime.now().strftime('%Y%m%d')}.csv", index=False)
